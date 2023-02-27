9fb6e2c8e0f1bfdd39644473972c9b8641183e68
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-02-27 11:59:25 -0300

    feat(app): browserRouter
    Adicionando o provider do react-router-dom para funcionalidade de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button/Button'
+import { BrowserRouter } from 'react-router-dom'
 import { GlobalStyle } from './global'
 import { DefaultThemes } from './styles/themes/default'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={DefaultThemes}>
+
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
       <GlobalStyle />
     </ThemeProvider>
   )
