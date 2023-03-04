e48870592384c94c2c9fb1d5f770fb669cc4e239
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-03-04 13:30:37 -0300

    feat(history): status
    Adicionando status de concluído ou não na tabela

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -57,3 +57,26 @@ table {
     }
 }
 `
+
+const STATUS_COLOR = {
+    yellow: 'yellow',
+    red: 'red',
+    green: 'green'
+}as const
+interface StatusProps {
+    statusColor: keyof typeof STATUS_COLOR
+}
+
+export const Status = styled.span<StatusProps>`
+display: flex;
+align-items: center;
+gap: 0.5rem;
+
+&::before {
+    content: "";
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 9999px;
+    background-color: ${props => props.theme[STATUS_COLOR[props.statusColor]]};
+}
+`
