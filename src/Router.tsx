30532a7bb387311b707ba6564bc3a4eb77af4bb5
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-03-04 12:28:50 -0300

    style(rotuer.tsx): alterando o import do componente History

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Route } from 'react-router'
 import { Routes } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
-import { History } from './pages/History'
+import { History } from './pages/History/History'
 import { Home } from './pages/Home/Home'
 
 export function Router() {
