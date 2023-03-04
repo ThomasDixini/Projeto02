e20f08cfd59dcfb1f47e7cecba9fd2f5e064e017
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-03-04 16:13:45 -0300

    feat(home): validando formulários
    Validando formulários com useForm e com a biblioteca zod

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,17 +7,46 @@ import {
   TaskInput,
 } from './styles'
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import zod from 'zod'
+
+const newCycleFormValidationSchema = zod.object(
+  {
+    task: zod
+    .string()
+    .min(1, "Informe a tarefa"),
+    minutesAmount: zod
+    .number()
+    .min(5, "O ciclo precisa ser de no minímo 5")
+    .max(60, "O máximo precisa ser de no máximo 60")
+  }
+)
 
 export function Home() {
+
+  const { register, handleSubmit, watch, formState } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema)
+  });
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
+  }
+
+  const task = watch('task')
+  const isTaskDisabled = !task;
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task"> Vou trabalhar em </label>
           <TaskInput
             id="task"
             placeholder="De um nome para seu projeto"
             list="projects-suggestions"
+            {...register('task')}
           />
 
           <datalist id="projects-suggestions">
@@ -34,6 +63,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', {valueAsNumber: true})}
           />
 
           <span> minutos </span>
@@ -46,7 +76,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton type="submit">
+        <StartCountdownButton type="submit" disabled={isTaskDisabled}>
           <Play size={24} />
           Começar
         </StartCountdownButton>
